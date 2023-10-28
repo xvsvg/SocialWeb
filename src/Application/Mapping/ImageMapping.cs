08b10ec551e91d5e8d40b7aedbda20822682e6c1
xvsvg <pobedotriples@gmail.com>
2023-10-28 22:16:59 +0300

    feat: image mapping

---
+++ src/Application/Mapping/ImageMapping.cs
@@ -0,0 +1,16 @@
+﻿using Contracts.Images;
+using Domain.Core.Image;
+
+namespace Application.Mapping;
+
+public static class ImageMapping
+{
+    public static ImageResponse ToDto(this Image image)
+    {
+        return new ImageResponse
+        {
+            Id = image.Id,
+            Name = image.Filename
+        };
+    }
+}
